a1b67bce0f242ee0836e917f4e8fb3ac28340ad7
Mandeep Gill <mandy@Mandeeps-MacBook-Air.local>
2018-04-08 23:55:41 -0500

    Added js files

---
+++ javascript/index26.js
@@ -0,0 +1,13 @@
+let city= "Winnipeg";
+const weather= "cold";
+let touristPlaces= null;
+let population= 100000;
+let metroCity= false;
+let hasAliens= undefined;
+
+console.log('city', city + ' '+ typeof city);
+console.log('weather', weather + ' '+ typeof weather);
+console.log('touristPlaces', touristPlaces + ' '+ typeof touristPlaces);
+console.log('population', population + ' '+ typeof population);
+console.log('metrocity', metroCity + ' '+ typeof metroCity);
+console.log('hasAliens', hasAliens + ' '+ typeof hasAliens);
